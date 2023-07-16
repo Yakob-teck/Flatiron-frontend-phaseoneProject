3d15aeb859f91dca72f172391ad7e710c63eca3f
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-07-15 22:34:54 -0600

    css  file updaupdating html t

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-console.log(connected);
-function toggleMenu() {
-   const menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-  
-    const menuIcon = document.querySelector('.menu-icon');
-     menu = document.getElementById('menu');
-  
-    // Toggle the class to change the icon to a close button
-    menuIcon.classList.toggle('');
-  
-    // Toggle the menu visibility
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-  }
-  
-  
+
+
