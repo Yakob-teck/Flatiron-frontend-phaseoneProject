77141fcb849dd7907c6366d4333a8de125694a56
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-07-17 13:04:34 -0600

    update css

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 console.log("connected");
 
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  const menuIcon = document.querySelector('.menu-icon');
-
-  // Toggle the class to change the icon to a close button
-  menuIcon.classList.toggle('close');
-
-  // Toggle the menu visibility
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
-
 const imageContainer = document.querySelector('.image-container');
 const imageElement = document.getElementById('image');
 
@@ -20,7 +9,6 @@ const images = [
   'file:///Users/yakobasebe/Pictures/2021-10-14.jpg',
   'file:///Users/yakobasebe/Pictures/IMG-20211217-WA0008.jpg',
   'file:///Users/yakobasebe/Pictures/2020-04-29%20(1).jpg'
- 
 ];
 
 let currentIndex = 0;
